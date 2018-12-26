cc8c498ec17b69fc519ebe6fda6130b4cde23131
taroo0ooq <44965490+taroo0ooq@users.noreply.github.com>
2018-12-26 17:41:25 +0300

    Add files via upload

---
+++ AzureSBGSP2016HA/SP2016_HA_Scripts/add-dns.ps1
@@ -0,0 +1,4 @@
+﻿Import-Module DNSServer
+
+Add-DnsServerResourceRecordA -Name "Portal" -ZoneName "bhc.sa" -AllowUpdateAny -IPv4Address "10.0.1.100" -TimeToLive 01:00:00 -ComputerName "AD1.bhc.sa"
+Add-DnsServerResourceRecordA -Name "OneDrive" -ZoneName "bhc.sa" -AllowUpdateAny -IPv4Address "10.0.1.100" -TimeToLive 01:00:00 -ComputerName "AD1.bhc.sa"
